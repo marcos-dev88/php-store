910bcb26886282b5f9eaa6d41f80e45aa3634d1a
Marcos Vitor <mv.piesske@gmail.com>
2021-12-22 21:35:54 -0300

    Add index file with all API calls

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+
+require '../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+$app->setBasePath('/api/v1');
+
+require('../api/v1/user.php');
+require('../api/v1/store.php');
+require('../api/v1/login.php');
+
+
+try {
+    $app->run();
+}catch (Exception $e) {
+    die(json_encode(
+        [
+            "status" => "failed",
+            "message" => "This action is not allowed",
+            "exception_message" => $e
+        ]
+    ));
+}
